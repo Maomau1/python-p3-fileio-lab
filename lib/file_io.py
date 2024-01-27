eb8a2e8ff758fb7da8b583397072f8f0f52de30a
maurice diong <mdiong00@gmail.com>
2024-01-26 21:49:24 -0500

    with statement allows us to open the file but then we still have to do something (or nothing)

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', encoding = 'utf-8', mode = 'w')as text_file:
+        text_file.write(f"{file_content}")
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", encoding = 'utf-8', mode = 'a') as text_file:
+        text_file.write(f"{append_content}")
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8') as text_file:
+        # breakpoint()
+        return text_file.read()
